f7962002812abfbcc003ace60616eb3e1fcc430d
Sören Oldag <soeren_oldag@freenet.de>
2017-07-11 14:00:09 +0200

    Fix flatten method.

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+
+
 def first(gen, default_value=None):
     try:
         return next(gen)
@@ -8,6 +11,6 @@ def first(gen, default_value=None):
 def flatten(l, remove_duplicates=False):
     flat_list = [item for sublist in l for item in sublist]
     if remove_duplicates:
-        flat_list = set(flat_list)
+        flat_list = list(OrderedDict.fromkeys(flat_list))
 
     return flat_list
